e6382c877268f7a136d8ecec3abf2db8d2b871fa
vflyson <vflyson@users.noreply.github.com>
2017-05-21 12:00:39 -0400

    First commit

---
+++ Makefile
@@ -0,0 +1,28 @@
+# Makefile for xkeyminator - an automated keyboard input tool for X11.
+# See LICENSE file for copyright and license details.
+
+CFLAGS = -std=c99 -s -pedantic -Wall -Wextra -Wfatal-errors -pedantic-errors -O3 -D_XOPEN_SOURCE=500 -D_POSIX_C_SOURCE=200809L
+CC     = gcc $(CFLAGS)
+
+LIBS = -lX11
+PROG = xkeyminator
+
+all: $(PROG)
+
+$(PROG):
+	    $(CC) -o $(PROG) $(PROG).c $(LIBS)
+
+clean:
+	    rm -f $(PROG)
+
+install: all
+	@echo installing executable file to ${DESTDIR}${PREFIX}/bin
+	@mkdir -p ${DESTDIR}${PREFIX}/bin
+	@cp -f $(PROG) ${DESTDIR}${PREFIX}/bin
+	@chmod 755 ${DESTDIR}${PREFIX}/bin/$(PROG)
+
+uninstall:
+	@echo removing executable file from ${DESTDIR}${PREFIX}/bin
+	@rm -f ${DESTDIR}${PREFIX}/bin/$(PROG)
+
+.PHONY: all clean install uninstall
---
+++ xkeyminator.c
@@ -0,0 +1,143 @@
+#include <string.h>
+#include <time.h>
+
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+
+#define KEYPRESS_DELAY 2
+#define KEYPRESS_WAIT  10
+
+void msleep(int milliseconds)
+{
+    struct timespec ts;
+
+    ts.tv_sec = milliseconds / 1000;
+    ts.tv_nsec = (milliseconds % 1000) * 1000000;
+
+    nanosleep(&ts, NULL);
+}
+
+XKeyEvent createKeyEvent(Display *display, Window window, Window rootWin,
+                         char press, int keycode, unsigned int modifiers)
+{
+    XKeyEvent event;
+
+    event.display     = display;
+    event.window      = window;
+    event.root        = rootWin;
+    event.subwindow   = None;
+    event.time        = CurrentTime;
+    event.x           = 1;
+    event.y           = 1;
+    event.x_root      = 1;
+    event.y_root      = 1;
+    event.same_screen = True;
+    event.keycode     = XKeysymToKeycode(display, keycode);
+    event.state       = modifiers;
+
+    if (press)
+        event.type = KeyPress;
+    else
+        event.type = KeyRelease;
+
+    return event;
+}
+
+char requiresShiftModifier(char c)
+{
+    switch (c)
+    {
+        case '~':
+        case '!':
+        case '@':
+        case '#':
+        case '$':
+        case '%':
+        case '^':
+        case '&':
+        case '*':
+        case '(':
+        case ')':
+        case '_':
+        case '+':
+        case 'Q':
+        case 'W':
+        case 'E':
+        case 'R':
+        case 'T':
+        case 'Y':
+        case 'U':
+        case 'I':
+        case 'O':
+        case 'P':
+        case '{':
+        case '}':
+        case '|':
+        case 'A':
+        case 'S':
+        case 'D':
+        case 'F':
+        case 'G':
+        case 'H':
+        case 'J':
+        case 'K':
+        case 'L':
+        case ':':
+        case '"':
+        case 'Z':
+        case 'X':
+        case 'C':
+        case 'V':
+        case 'B':
+        case 'N':
+        case 'M':
+        case '<':
+        case '>':
+        case '?':
+            return 1;
+    }
+
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    Display *display = XOpenDisplay(0);
+
+    if (display == NULL) {
+        return -1;
+    }
+
+    Window rootWin = XDefaultRootWindow(display);
+
+    Window focWin;
+    int revert;
+    XGetInputFocus(display, &focWin, &revert);
+
+    for (int c = 1; c < argc; c++) {
+        char *string = argv[c];
+
+        for (int i = 0, ilen = strlen(string); i < ilen; i++) {
+            char keycode = string[i];
+            unsigned int modifiers = 0;
+
+            if (requiresShiftModifier(keycode)) {
+                modifiers = ShiftMask;
+            }
+
+            XKeyEvent event = createKeyEvent(display, focWin, rootWin, 1, keycode, modifiers);
+            XSendEvent(event.display, event.window, True, KeyPressMask, (XEvent*)&event);
+
+            msleep(KEYPRESS_DELAY);
+
+            event = createKeyEvent(display, focWin, rootWin, 0, keycode, modifiers);
+            XSendEvent(event.display, event.window, True, KeyPressMask, (XEvent*)&event);
+
+            msleep(KEYPRESS_WAIT);
+        }
+    }
+
+    XCloseDisplay(display);
+
+    return 0;
+}
