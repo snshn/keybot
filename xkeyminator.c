0448366c4978278523a38660992470a70147f12d
vflyson <vflyson@users.noreply.github.com>
2017-05-21 22:19:45 -0400

    Implement input from stdin

--- xkeyminator.c
+++ xkeyminator.c
@@ -1,10 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
 #include <string.h>
 #include <time.h>
 
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
 
-#define KEYPRESS_DELAY 2
+#define KEYPRESS_DELAY 3
 #define KEYPRESS_WAIT  10
 
 void msleep(int milliseconds)
@@ -17,15 +19,15 @@ void msleep(int milliseconds)
     nanosleep(&ts, NULL);
 }
 
-XKeyEvent createKeyEvent(Display *display, Window window, Window rootWin,
+XKeyEvent createKeyEvent(Display *display, Window *window, Window *rootWin,
                          int type, int keycode, unsigned int modifiers)
 {
     XKeyEvent event;
 
     event.type        = type;
     event.display     = display;
-    event.window      = window;
-    event.root        = rootWin;
+    event.window      = *window;
+    event.root        = *rootWin;
     event.subwindow   = None;
     event.time        = CurrentTime;
     event.x           = 1;
@@ -96,6 +98,24 @@ char requiresShiftModifier(char c)
     return 0;
 }
 
+void simKeyPress(Display *display, Window *focWin, Window *rootWin,
+                  int keycode, unsigned int modifiers)
+{
+    XKeyEvent event = createKeyEvent(display, focWin, rootWin,
+                                     KeyPress, keycode, modifiers);
+    XSendEvent(event.display, event.window, True,
+               KeyPressMask, (XEvent*)&event);
+
+    msleep(KEYPRESS_DELAY);
+
+    event = createKeyEvent(display, focWin, rootWin,
+                           KeyRelease, keycode, modifiers);
+    XSendEvent(event.display, event.window, True,
+               KeyPressMask, (XEvent*)&event);
+
+    msleep(KEYPRESS_WAIT);
+}
+
 int main(int argc, char **argv)
 {
     Display *display = XOpenDisplay(0);
@@ -110,36 +130,50 @@ int main(int argc, char **argv)
     int revert;
     XGetInputFocus(display, &focWin, &revert);
 
-    for (int c = 1; c < argc; c++) {
-        char *string = argv[c];
+    char keycode;
+    unsigned int modifiers = 0;
+    char *string;
 
-        for (int i = 0, ilen = strlen(string); i < ilen; i++) {
-            char keycode = string[i];
-            unsigned int modifiers = 0;
+    if (isatty(fileno(stdin)) && argc < 2) { // NULL stdin, no arguments
+        fprintf(stderr, "\n  Usage: %s \"string to type\"\n"
+              "         cat ~/.ssh/id_rsa.pub | %s\n"
+              "         %s \"string to type\" < ~/.ssh/id_rsa.pub\n\n"
+              , argv[0], argv[0], argv[0]);
+        return -1;
+    }
+
+    if (!isatty(fileno(stdin))) {
+        while (read(0, &keycode, 1) > 0) {
+            modifiers = 0;
 
             if (requiresShiftModifier(keycode)) {
                 modifiers = ShiftMask;
             }
 
-            XKeyEvent event = createKeyEvent(display, focWin, rootWin, KeyPress,
-                                             keycode, modifiers);
-            XSendEvent(event.display, event.window, True, KeyPressMask,
-                       (XEvent*)&event);
+            simKeyPress(display, &focWin, &rootWin, keycode, modifiers);
+        }
+    }
 
-            msleep(KEYPRESS_DELAY);
+    for (int c = 1; c < argc; c++) {
+        string = argv[c];
 
-            event = createKeyEvent(display, focWin, rootWin, KeyRelease,
-                                   keycode, modifiers);
-            XSendEvent(event.display, event.window, True, KeyPressMask,
-                       (XEvent*)&event);
+        for (int i = 0, ilen = strlen(string); i < ilen; i++) {
+            keycode = string[i];
+            modifiers = 0;
+
+            if (requiresShiftModifier(keycode)) {
+                modifiers = ShiftMask;
+            }
+
+            simKeyPress(display, &focWin, &rootWin, keycode, modifiers);
 
             string[i] = '\0';
-
-            msleep(KEYPRESS_WAIT);
         }
     }
 
     XCloseDisplay(display);
+    keycode = '\0';
+    modifiers = 0;
 
     return 0;
 }
